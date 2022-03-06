c348b02b01650849f1765f4f92f30af130bf1a96
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2022-03-06 12:59:52 +0300

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import BookSchedule from './components/Booklist';
+
+
+function App() {
+  return (
+    <div className="App">
+    <h1>📚 Bucheli Book Scheduler📚</h1>
+     <BookSchedule />
+    </div>
+  );
+}
+
+export default App;
